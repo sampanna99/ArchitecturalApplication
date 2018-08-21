c4973e4c6022ce151e361bdb74f0c2de100479de
sampanna pokhrel <sampanna99@gmail.com>
2018-08-20 22:05:34 -0400

    Add project files.

---
+++ ArchitecturalApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArchitecturalApplication.Startup))]
+namespace ArchitecturalApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
